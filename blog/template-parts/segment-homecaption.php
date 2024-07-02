67a8e58d36a421a1fb2874242b5b39bbc9b57cbc
Md Shayon <mdshayon0@gmail.com>
2024-07-02 17:55:36 +0600

    Reformat using TypeScript, and SASS and make menu responsive

--- blog/template-parts/segment-homecaption.php
+++
@@ -1,8 +0,0 @@
-
-
-<div class="container caption-main d-flex justify-content-center align-items-center">
-    <div class="col-md-6 col-sm-12 text-white text-center">
-        <h1 class="text-uppercase heading-caption text-shadow"> h1 <?php echo get_theme_mod('itssportstime_caption_title_setting'); ?></h1>
-        <p > p <?php echo get_theme_mod('itssportstime_caption_description_setting'); ?></p>
-    </div>
-</div>
